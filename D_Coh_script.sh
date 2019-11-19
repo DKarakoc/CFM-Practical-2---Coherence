440166f8cdb0c5eab8fc1a05964c8b4d9e20e02c
Denis Karakoç <denis.karakoc@gmail.com>
2019-11-19 12:20:52 +0100

    script now generates graphs automatically

--- D_Coh_script.sh
+++ D_Coh_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 counter=1
-until [ $counter -gt 100 ] # change this number for how many times you want to run it
+until [ $counter -gt 10 ] # change this number for how many times you want to run it
 do
 	./D_Coh.py >> D_Coh_Results.txt
 	((counter++))
 done
+
+./diagrams.py
 
